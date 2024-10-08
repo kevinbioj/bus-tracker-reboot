f7c6f2c489e5cae06a769684eddfc774993d3a69
Kevin BIOJOUT <biojoutk1106@gmail.com>
2024-10-08 19:31:33 +0200

    refactor: move from csv-parse to papaparse (huge performance improvement)

--- gtfs-processor/src/utils/csv-reader.ts
+++ gtfs-processor/src/utils/csv-reader.ts
@@ -1,5 +1,5 @@
-import { parse } from "csv-parse";
 import { createReadStream } from "node:fs";
+import Papa from "papaparse";
 
 export type CsvRecord<Required extends string = never, Optional extends string = never> = {
   [key in Required]: string;
@@ -10,19 +10,41 @@ type ReadCsvOptions = {
   encoding?: BufferEncoding;
 };
 
-export function readCsv<T extends CsvRecord<string>>(
+async function readFirstBytes(path: string, size: number) {
+  const chunks = [];
+  for await (const chunk of createReadStream(path, { start: 0, end: size - 1 })) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks);
+}
+
+export async function readCsv<T extends CsvRecord<string>>(
   path: string,
   onRecord: (record: T) => void,
   { delimiter, encoding }: ReadCsvOptions = {},
 ) {
-  const readStream = createReadStream(path, { encoding });
-  const parser = readStream.pipe(
-    parse({ bom: true, columns: true, delimiter, skipEmptyLines: true }),
-  );
-
-  parser.on("data", onRecord);
-  return new Promise((resolve, reject) => {
-    parser.once("end", resolve);
-    parser.once("error", reject);
+  const firstBytes = await readFirstBytes(path, 3);
+  const hasBom = firstBytes.toString().charCodeAt(0) === 0xfeff;
+  const readStream = createReadStream(path, { encoding, start: hasBom ? 3 : 0 });
+  return new Promise<void>((resolve, reject) => {
+    Papa.parse<T>(readStream, {
+      complete: () => resolve(),
+      delimiter,
+      encoding,
+      error: (error) => reject(error),
+      header: true,
+      skipEmptyLines: true,
+      step: (results) => onRecord(results.data),
+      worker: true,
+    });
   });
+  // const parser = readStream.pipe(
+  //   parse({ bom: true, columns: true, delimiter, skipEmptyLines: true }),
+  // );
+
+  // parser.on("data", onRecord);
+  // return new Promise((resolve, reject) => {
+  //   parser.once("end", resolve);
+  //   parser.once("error", reject);
+  // });
 }
